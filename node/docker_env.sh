e9211f23762dff5092ecefecc03251a5e8abec51
c-zhavih <ihar.zhavarankau.consultant@consumer.org>
2018-04-11 09:38:18 -0400

    initial

---
+++ node/docker_env.sh
@@ -0,0 +1,4 @@
+ export DOCKER_API_VERSION="1.23";
+ export DOCKER_HOST="tcp://192.168.99.100:2376";
+ export DOCKER_CERT_PATH="/home/phoenix/.minikube/certs";
+ export DOCKER_TLS_VERIFY="1";
---
+++ node/server.mjs
@@ -0,0 +1,11 @@
+import http from 'http';
+ 
+const handleRequest = (request, response) => {
+	console.log('Received url: ' + request.url);
+	response.writeHead(200);
+	response.end('Hello new ')
+};
+
+const www = http.createServer(handleRequest);
+
+www.listen(8080);
